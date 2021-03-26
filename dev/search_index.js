c9889a257c7becbd357b0a0d02f0ebf0492fa3ee
Documenter.jl <documenter@juliadocs.github.io>
2021-03-26 12:46:25 +0000

    build based on bcbcd28

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Interp1d","category":"page"},{"location":"#Interp1d","page":"Home","title":"Interp1d","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Interp1d.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Interp1d]","category":"page"}]
+}
