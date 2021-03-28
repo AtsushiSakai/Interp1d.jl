db13c14d8d4c7df2289b46ff5c5470557119565f
Atsushi Sakai <asakai.amsl+github@gmail.com>
2021-03-28 14:31:18 +0900

    add copy optional arguments

--- src/Nearest.jl
+++ src/Nearest.jl
@@ -10,31 +10,32 @@ abstract type Nearest <: InterpMode end
 Returns a nearest interpolation function `f(x)` based on interpolated points `xi` and `yi`.
 
 # Examples
-This is a simple example:
+This is a simple interpolation example:
 ```jldoctest
 julia> using Interp1d
 
-julia> x = [-1.0, 0.0, 3.0/2.0];
+julia> xi = [-1.0, 0.0, 3.0/2.0];
 
-julia> y = [2.0, 1.0, 3.0];
+julia> yi = [2.0, 1.0, 3.0];
 
-julia> f = interp(x, y, Nearest);
+julia> f = interp(xi, yi, Nearest);
 
 julia> f(1.0)
 3.0
 ```
 
-This is a simple example:
+`yi` can be matrix for multi univerate interpolations.
+The number of rows of `yi` must be equal to the length of `xi`.
 
 ```jldoctest
 julia> using Interp1d
 
-julia> x = [-1.0, 0.0, 3.0/2.0];
+julia> xi = [-1.0, 0.0, 3.0/2.0];
 
-julia> y = [ 2.0  1.0 3.0;
-            -1.0 -4.4 2.0];
+julia> yi = [ 2.0  1.0 3.0;
+             -1.0 -4.4 2.0];
 
-julia> f = interp(x, y, Nearest);
+julia> f = interp(xi, yi, Nearest);
 
 julia> f(0.0)
 2-element Vector{Float64}:
@@ -42,21 +43,29 @@ julia> f(0.0)
  -4.4
 ```
 
-
-
 """
-function interp(xi::Vector{T}, yi::Vector{T}, ::Type{Nearest}) where T
+function interp(xi::Vector{T}, yi::Vector{T}, ::Type{Nearest};
+                copy=true, bounds_error=false, assume_sorted=false) where T
+    _xi, _yi = _prepare_input(xi, yi, copy, bounds_error, assume_sorted)
     
-    f(x) = yi[_searchsortednearest(xi, x)]
+    f(x) = _yi[_searchsortednearest(_xi, x)]
         
     return f
 end
 
-function interp(xi::Vector{T}, yi::Matrix{T}, ::Type{Nearest}) where T
+function interp(xi::Vector{T}, yi::Matrix{T}, ::Type{Nearest};
+                copy=true, bounds_error=false, assume_sorted=false) where T
+    _xi, _yi = _prepare_input(xi, yi, copy, bounds_error, assume_sorted)
     
-    f(x) = yi[:, _searchsortednearest(xi, x)]
+    f(x) = _yi[:, _searchsortednearest(_xi, x)]
         
     return f
+end
+
+function _prepare_input(xi, yi, copy, bounds_error, assume_sorted)
+    _xi, _yi = copy ? (deepcopy(xi), deepcopy(yi)) : (xi, yi)
+
+    return _xi, _yi
 end
 
 function _searchsortednearest(a,x)
--- test/NearestTest.jl
+++ test/NearestTest.jl
@@ -24,3 +24,16 @@ end
     @test f(Inf) == [3.0, 2.0]
     @test f(0) == [1.0, -4.4]
 end
+
+@testset "copy test" begin
+    x = [-1.0, 0.0, 3.0/2.0]
+    y = [2.0, 1.0, 3.0]
+    f = interp(x, y, Nearest, copy=true);
+    y[2] = 10.0
+    @test f(0) == 1.0
+    f = interp(x, y, Nearest, copy=false);
+    y[2] = -10.0
+    @test f(0) == -10.0
+end
+
+
